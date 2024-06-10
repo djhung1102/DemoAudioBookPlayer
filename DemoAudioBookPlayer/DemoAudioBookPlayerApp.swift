2b5b5709b9a5c4fba4009743d9482fe8864c4e5e
Nguyen Manh Hung <manhhunghd2000@gmail.com>
2024-06-10 16:25:31 +0700

    Initial Commit

---
+++ DemoAudioBookPlayer/DemoAudioBookPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoAudioBookPlayerApp.swift
+//  DemoAudioBookPlayer
+//
+//  Created by Nguyễn Mạnh Hùng on 10/6/24.
+//
+
+import SwiftUI
+
+@main
+struct DemoAudioBookPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
