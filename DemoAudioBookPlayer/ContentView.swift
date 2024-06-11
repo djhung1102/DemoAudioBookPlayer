0197453425db2d71cbe058d4fcd10c839b54e01f
Nguyen Manh Hung <manhhunghd2000@gmail.com>
2024-06-11 10:03:28 +0700

    push code

--- DemoAudioBookPlayer/ContentView.swift
+++ DemoAudioBookPlayer/ContentView.swift
@@ -6,18 +6,94 @@
 //
 
 import SwiftUI
+import SwiftData
+//import Model
+import AudioBookPlayer
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+    @Environment(AudioBookPlayerManager.self) var playerManager
+    
+    @State var selection = 0
+    
+    
+    public var body: some View {
+        TabView(selection: $selection) {
+            ZStack {
+                Color(.systemBackground)
+                    .ignoresSafeArea()
+                
+                VStack {
+                    Button(action: {
+                        print("Play0")
+                        Task {
+                            await playerManager.play(
+                                Book.examples[0], player: nil, currentSpeed: 1
+                            )
+                        }
+                    }, label: {
+                        if playerManager.isFetching {
+                            ProgressView()
+                        } else {
+                            Text("Play0")
+                        }
+                    })
+                    .padding()
+                    
+//                    Button(action: {
+//                        print("Play1")
+//                        Task {
+//                            await playerManager.play(
+//                                Book.examples[1], player: nil, currentSpeed: 1
+//                            )
+//                        }
+//                    }, label: {
+//                        if playerManager.isFetching {
+//                            ProgressView()
+//                        } else {
+//                            Text("Play1")
+//                        }
+//                    })
+//                    .padding()
+                    
+                    Text("Play1")
+                        .onTapGesture {
+                            
+                            Task {
+//                                    await audioBookPlayerManager.play(libraryBook.book, displayInMode: .full)
+                                await playerManager.play(
+                                    Book.examples[1], player: nil, currentSpeed: 1
+                                )
+                            }
+                        }
+                }
+            }
+            .tabItem {
+                Image(systemName: "house.fill")
+                Text("Home")
+            }
+            .tag(0)
+            
+            LibraryView()
+                .tabItem {
+                    Image(systemName: "gear")
+                    Text("Books")
+                }
+                .tag(1)
+            
+            Color(.red)
+                .tabItem {
+                    Image(systemName: "gear")
+                    Text("Settings")
+                }
+                .tag(2)
         }
-        .padding()
-    }
-}
+        .overlay {
+            if playerManager.displayInMode != nil {
+                AudioBookPlayer()
+            }
+        }
+
+    }}
 
 #Preview {
     ContentView()
--- DemoAudioBookPlayer/DemoAudioBookPlayerApp.swift
+++ DemoAudioBookPlayer/DemoAudioBookPlayerApp.swift
@@ -6,12 +6,30 @@
 //
 
 import SwiftUI
+import SwiftData
+import AudioBookPlayer
 
 @main
 struct DemoAudioBookPlayerApp: App {
+    
+    var sharedModelContainer: ModelContainer = {
+            let schema = Schema([
+                LibraryBook.self
+            ])
+            let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+            do {
+                return try ModelContainer(for: schema, configurations: [modelConfiguration])
+            } catch {
+                fatalError("Could not create ModelContainer: \(error)")
+            }
+        }()
+
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+        .modelContainer(sharedModelContainer)
+        .environment(AudioBookPlayerManager(modelContext: sharedModelContainer.mainContext))
     }
 }
